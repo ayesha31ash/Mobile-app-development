8d1e819ca0a178dbb96535883760f240395ae06c
ayesha31ash <43003827+ayesha31ash@users.noreply.github.com>
2023-08-13 12:31:43 +0530

    Add files via upload

---
+++ TestGetResult/app/src/main/java/com/example/testgetresult/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.testgetresult;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void goToSecond(View view){
+        Intent intent=new Intent(this,SeondActivity.class);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ TestGetResult/app/src/main/java/com/example/testgetresult/SeondActivity.java
@@ -0,0 +1,20 @@
+package com.example.testgetresult;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SeondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seond);
+    }
+    public void sendData(View v){
+        Intent intent = new Intent();
+        String
+    }
+}
